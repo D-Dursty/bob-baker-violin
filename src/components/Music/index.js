2c944bae2ade5bd9770580f725eb62d59d2c832b
D-Dursty <devondahlia@gmail.com>
2024-02-13 20:48:34 -0800

    video update and responsive code edited

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,16 +1,57 @@
-import React from 'react'
-import Iframe from 'react-iframe'
+import React from "react";
+import Iframe from "react-iframe";
 
-import './style.css'
+import "./style.css";
 
-export default  function Music() {
-    return(
-        <div className='vidContainer'>
-            <div className='vidContainerOne'><Iframe className='vidOne' src="https://www.youtube.com/embed/566nN9FefM4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen scrolling='auto'></Iframe></div>
-            <div className='vidContainerTwo'><Iframe  className='vidTwoTwo' src="https://www.youtube.com/embed/LRJe_QT1voA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></Iframe></div>
-        </div>
-    )
-}
+
+ export default function Music() {
+  return (
+    <div className="vidContainer">
+      <div className="vidContainerOne">
+        <Iframe
+          className="vidOne"
+          src="https://www.youtube.com/embed/566nN9FefM4"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          scrolling="auto"
+        ></Iframe>
+      </div>
+      <div className="vidContainerTwo">
+        <Iframe
+          className="vidTwoTwo"
+          src="https://www.youtube.com/embed/LRJe_QT1voA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          scrolling="auto"
+        ></Iframe>
+      </div>
+      <div className='vidContainerThree'>
+      <iframe
+        className='vidThree'
+        src="https://www.youtube.com/embed/dV11I3pEpF4"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        ></iframe>
+    </div>
+    <div className='vidContainerFour'>
+      <iframe
+        className='vidFour'
+        src="https://www.youtube.com/embed/b29wNydeSZo"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen     
+        ></iframe>
+    </div>
+    </div>
+  );
+} 
 
 //<iframe width="560" height="315" src="https://www.youtube.com/embed/566nN9FefM4?si=mW7jjgJQnip4xkwk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
